a4904ea311c5c35fe4d3af11c9b530ac9a35a811
ngardunia <nickrhyse@gmail.com>
2023-02-08 16:59:24 -0500

    created code for Category.js and category-routes.js

--- models/Category.js
+++ models/Category.js
@@ -5,8 +5,25 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
+  // define columns
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
